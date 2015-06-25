97edea0ecdda78aebb9a1d2e409db36d33d6cf10
carlos <carlosthe19916@gmail.com>
2015-06-24 22:38:43 -0500

    client test

---
+++ cooperativa-testsuite/cooperativa-testsuite-client/src/test/java/org/sistcoop/cooperativa/services/managers/BovedasResourceTest.java
@@ -0,0 +1,10 @@
+package org.sistcoop.cooperativa.services.managers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BovedasResourceTest {
+
+	Logger log = LoggerFactory.getLogger(BovedasResourceTest.class);
+
+}
