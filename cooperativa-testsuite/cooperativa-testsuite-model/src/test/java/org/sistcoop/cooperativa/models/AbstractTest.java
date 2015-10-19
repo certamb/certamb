840736b06e5625b74b756b9468cb1c20eabe1876
carlos <carlos@carlos-Satellite-S55-A>
2015-10-18 22:14:00 -0500

    add representation

--- cooperativa-testsuite/cooperativa-testsuite-model/src/test/java/org/sistcoop/cooperativa/models/AbstractTest.java
+++ cooperativa-testsuite/cooperativa-testsuite-model/src/test/java/org/sistcoop/cooperativa/models/AbstractTest.java
@@ -18,6 +18,7 @@ import org.sistcoop.cooperativa.models.jpa.search.SearchCriteriaJoinModel;
 import org.sistcoop.cooperativa.models.search.SearchCriteriaModel;
 import org.sistcoop.cooperativa.models.utils.ModelToRepresentation;
 import org.sistcoop.cooperativa.provider.Provider;
+import org.sistcoop.cooperativa.representations.idm.BovedaRepresentation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -25,29 +26,33 @@ import org.slf4j.LoggerFactory;
 @UsingDataSet("empty.xml")
 public abstract class AbstractTest {
 
-	protected Logger log = LoggerFactory.getLogger(AbstractTest.class);
+    protected Logger log = LoggerFactory.getLogger(AbstractTest.class);
 
-	@Deployment
-	public static WebArchive createDeployment() {
-		File[] dependencies = Maven.resolver().resolve("org.slf4j:slf4j-log4j12:1.7.12").withoutTransitivity().asFile();
+    @Deployment
+    public static WebArchive createDeployment() {
+        File[] dependencies = Maven.resolver().resolve("org.slf4j:slf4j-log4j12:1.7.12").withoutTransitivity()
+                .asFile();
 
-		WebArchive war = ShrinkWrap.create(WebArchive.class, "test.war")
-				/** model-api **/
-				.addPackage(MapperConfigValidationException.class.getPackage())
-				.addPackage(BovedaModel.class.getPackage()).addPackage(OrigenTransaccionBovedaCaja.class.getPackage())
-				.addPackage(SearchCriteriaModel.class.getPackage()).addPackage(SearchCriteriaModel.class.getPackage())
-				.addPackage(ModelToRepresentation.class.getPackage()).addPackage(Provider.class.getPackage())
+        WebArchive war = ShrinkWrap.create(WebArchive.class, "test.war")
+                /** model-api **/
+                .addPackage(MapperConfigValidationException.class.getPackage())
+                .addPackage(BovedaRepresentation.class.getPackage())
+                .addPackage(BovedaModel.class.getPackage())
+                .addPackage(OrigenTransaccionBovedaCaja.class.getPackage())
+                .addPackage(SearchCriteriaModel.class.getPackage())
+                .addPackage(SearchCriteriaModel.class.getPackage())
+                .addPackage(ModelToRepresentation.class.getPackage()).addPackage(Provider.class.getPackage())
 
-				/** model-jpa **/
-				.addPackage(JpaBovedaProvider.class.getPackage()).addPackage(BovedaEntity.class.getPackage())
-				.addPackage(SearchCriteriaJoinModel.class.getPackage())
+                /** model-jpa **/
+                .addPackage(JpaBovedaProvider.class.getPackage()).addPackage(BovedaEntity.class.getPackage())
+                .addPackage(SearchCriteriaJoinModel.class.getPackage())
 
-				.addAsResource("META-INF/test-persistence.xml", "META-INF/persistence.xml")
-				.addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml").addAsWebInfResource("test-ds.xml");
+                .addAsResource("META-INF/test-persistence.xml", "META-INF/persistence.xml")
+                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml").addAsWebInfResource("test-ds.xml");
 
-		war.addAsLibraries(dependencies);
+        war.addAsLibraries(dependencies);
 
-		return war;
-	}
+        return war;
+    }
 
 }
