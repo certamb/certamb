5241811609bfdd940a00f3ff02275a2fdf967fed
Carlos <carlosthe19916@gmail.com>
2015-11-16 18:27:05 -0500

    add roles

--- certamb-integration/certamb-client/src/main/java/org/sistcoop/ceramb/admin/client/Roles.java
+++ certamb-integration/certamb-client/src/main/java/org/sistcoop/ceramb/admin/client/Roles.java
@@ -2,11 +2,15 @@ package org.sistcoop.ceramb.admin.client;
 
 public class Roles {
 
-	public final static String ver_bovedas = "ver-bovedas";
-	public final static String administrar_bovedas = "administrar-bovedas";
-	public final static String administrar_bovedas_agencia = "administrar-bovedas-agencia";
-	
-	public final static String ver_cajas = "ver-cajas";
-	public final static String administrar_cajas = "administrar-cajas";
-	public final static String administrar_cajas_agencia = "administrar-cajas-agencia";
+    public final static String ver_direcciones_regionales = "ver-direcciones-regionales";
+    public final static String administrar_direcciones_regionales = "administrar-direcciones-regionales";
+    public final static String eliminar_direcciones_regionales = "eliminar-direcciones-regionales";
+
+    public final static String ver_proyectos = "ver-bovedas";
+    public final static String administrar_proyectos = "administrar-bovedas";
+    public final static String administrar_proyectos_direccionRegional = "administrar-bovedas-agencia";
+
+    public final static String ver_cajas = "ver-cajas";
+    public final static String administrar_cajas = "administrar-cajas";
+    public final static String administrar_cajas_agencia = "administrar-cajas-agencia";
 }
