2cada701b4483543849330b9c379c91ba7d253d9
carlosthe19916 <carlosthe19916@gmail.com>
2015-11-16 21:15:14 -0500

    reconfigure roles

--- certamb-integration/certamb-client/src/main/java/org/sistcoop/ceramb/admin/client/Roles.java
+++ certamb-integration/certamb-client/src/main/java/org/sistcoop/ceramb/admin/client/Roles.java
@@ -2,15 +2,16 @@ package org.sistcoop.ceramb.admin.client;
 
 public class Roles {
 
-    public final static String ver_direcciones_regionales = "ver-direcciones-regionales";
-    public final static String administrar_direcciones_regionales = "administrar-direcciones-regionales";
-    public final static String eliminar_direcciones_regionales = "eliminar-direcciones-regionales";
+	public final static String ver_direcciones_regionales = "ver-direcciones-regionales";
+	public final static String administrar_direcciones_regionales = "administrar-direcciones-regionales";
+	public final static String eliminar_direcciones_regionales = "eliminar-direcciones-regionales";
 
-    public final static String ver_proyectos = "ver-proyectos";
-    public final static String administrar_proyectos = "administrar-proyectos";
-    public final static String administrar_proyectos_direccionRegional = "administrar-proyectos-direccionRegional";
+	public final static String ver_proyectos = "ver-proyectos";
+	public final static String administrar_proyectos = "administrar-proyectos";
+	public final static String administrar_proyectos_direccionRegional = "administrar-proyectos-direccionRegional";
+	public final static String eliminar_proyectos = "eliminar-proyectos";
 
-    public final static String ver_trabajadores = "ver-trabajadores";
-    public final static String administrar_trabajadores = "administrar-trabajadores";
-    public final static String eliminar_trabajadores = "eliminar-trabajadores";
+	public final static String ver_trabajadores = "ver-trabajadores";
+	public final static String administrar_trabajadores = "administrar-trabajadores";
+	public final static String eliminar_trabajadores = "eliminar-trabajadores";
 }
