5c46c6bcc0422304b92a49ac4bbcc74ff8dac115
carlosthe19916 <carlosthe19916@gmail.com>
2015-11-23 21:28:47 -0500

    add roles

--- certamb-integration/certamb-client/src/main/java/org/sistcoop/ceramb/admin/client/Roles.java
+++ certamb-integration/certamb-client/src/main/java/org/sistcoop/ceramb/admin/client/Roles.java
@@ -6,12 +6,16 @@ public class Roles {
 	public final static String administrar_direcciones_regionales = "administrar-direccionesRegionales";
 	public final static String eliminar_direcciones_regionales = "eliminar-direccionesRegionales";
 
+	public final static String ver_trabajadores = "ver-trabajadores";
+	public final static String administrar_trabajadores = "administrar-trabajadores";
+	public final static String administrar_trabajadores_direccionRegional = "administrar-trabajadores-direccionRegional";
+	public final static String eliminar_trabajadores = "eliminar-trabajadores";
+	public final static String eliminar_trabajadores_direccionRegional = "eliminar-trabajadores-direccionRegional";
+
 	public final static String ver_proyectos = "ver-proyectos";
 	public final static String administrar_proyectos = "administrar-proyectos";
 	public final static String administrar_proyectos_direccionRegional = "administrar-proyectos-direccionRegional";
 	public final static String eliminar_proyectos = "eliminar-proyectos";
+	public final static String eliminar_proyectos_direccionRegional = "eliminar-proyectos-direccionRegional";
 
-	public final static String ver_trabajadores = "ver-trabajadores";
-	public final static String administrar_trabajadores = "administrar-trabajadores";
-	public final static String eliminar_trabajadores = "eliminar-trabajadores";
 }
